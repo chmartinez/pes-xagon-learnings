04591aba996412f48fa4f1e84e8dfdbf3d16ecc3
christian <c.martinez@mytaxi.com>
2020-05-01 19:10:24 +0200

    feat(5th-attempt): Use Snap.svg to animate

--- js/5th-attempt.js
+++ js/5th-attempt.js
@@ -2,6 +2,6 @@
     loadDynamicScript('./js/libs/snap.svg-min.js', 'snapSvg', function () {
         const pesxagon = Snap('#snap-pes-xagon');
         let polygon = pesxagon.select('polygon');
-        polygon.animate({fill: 'white'}, 2000);
+        polygon.animate({ fill: 'white', points: [0, -90, 64.95, -37.5, 43.30, 25, 0, 50, -43.30, 25, -43.30, -25] }, 2000);
     });
 })();
